856641aebca227dc445fac92479d56eacc3dcaa2
Mayur Ramgir <mramgir@zonopact.com>
2019-11-02 12:05:19 +0530

    Add files via upload

---
+++ ch3/myjavascriptfile.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  	var mybutton = document.createElement("button");
+  	mybutton.innerHTML = "My Button";
+	mybutton.style.background = "blue";
+	mybutton.style.color = "white";
+
+  	document.body.appendChild(mybutton);
+}
